380c40b013ebb881c4208885a088b5d9d143061c
Gaetano Carbone <137958268+gwetano@users.noreply.github.com>
2025-04-12 13:14:38 +0200

    delete

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  onToggleDarkMode: (callback) => ipcRenderer.on('toggle-dark-mode', (event, isDark) => callback(isDark)),
-  onFileOpen: (callback) => ipcRenderer.on('file-open', callback),
-  onFileSave: (callback) => ipcRenderer.on('file-save', callback)
-});
